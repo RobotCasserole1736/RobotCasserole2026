e858d99067effc9ba5909c6096a284de51b93b11
ejwatkins <ewatkins987@gmail.com>
2026-02-05 20:51:56 -0600

    Missed accounting for corrupt or missing data and updated logic

--- fuelSystems/gameStateTracker.py
+++ fuelSystems/gameStateTracker.py
@@ -4,7 +4,7 @@ from wpilib import DriverStation, Timer
 class gameStateTracker(metaclass=Singleton):
   """
   Keeps track of hub active state
-  """
+  """ 
   def __init__(self) -> None:
     self.hubActive = True
     self.data = DriverStation.getGameSpecificMessage()
@@ -12,28 +12,34 @@ class gameStateTracker(metaclass=Singleton):
   def update(self) -> None:
     matchTime=Timer.getMatchTime()
     alliance = DriverStation.getAlliance()
-    if self.data is None:
-      self.data = DriverStation.getGameSpecificMessage()
 
-    # When on BLUE alliance
-    if alliance == DriverStation.Alliance.kBlue:
-      match self.data:
-        # Red alliance hub is INACTIVE first
-        case "R":
-          if ((matchTime >= 20 and matchTime <= 55) or
-              (matchTime >= 80 and matchTime <= 105) or
-              (matchTime >= 130)):
-            self.hubActive = True
-          else:
-            self.hubActive = False
-        # Blue alliance hub is INACTIVE first
-        case "B":
-          if ((matchTime >= 20 and matchTime <= 30) or
-              (matchTime >= 55 and matchTime <= 80) or
-              (matchTime >= 105)):
-            self.hubActive = True
-          else:
-            self.hubActive = False
+    # When data is received
+    if self.data:
+      # Data and alliance color match
+      if ((alliance == DriverStation.Alliance.kBlue and
+          self.data == "B") or
+         (alliance == DriverStation.Alliance.kRed and
+          self.data == "R")):
+        # Hub is active during shifts 2 and 4
+        if ((matchTime >= 20 and matchTime <= 30) or
+            (matchTime >= 55 and matchTime <= 80) or
+            (matchTime >= 105)):
+          self.hubActive = True
+        else:
+          self.hubActive = False
+      # Data and alliance color do NOT match
+      else:
+        # Hub is active during shifts 1 and 3
+        if ((matchTime >= 20 and matchTime <= 55) or
+            (matchTime >= 80 and matchTime <= 105) or
+            (matchTime >= 130)):
+          self.hubActive = True
+        else:
+          self.hubActive = False
+    # Data is not received or corrupt; assume hub is active
+    else:
+      self.hubActive = True
+      self.data = DriverStation.getGameSpecificMessage()
 
   # Return hub active boolean
   def getHubActive(self) -> bool:
