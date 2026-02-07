2d0cb129a16bc3d460abee7341a05a72648f4f03
Kyle <kwpadilla2008@gmail.com>
2026-02-07 10:46:41 -0600

    Test ideas for target class. used for having different data on what we need for each target.

--- fuelSystems/fuelSystemConstants.py
+++ fuelSystems/fuelSystemConstants.py
@@ -1,5 +1,7 @@
 from enum import Enum
 import math
+from wpimath.geometry import Pose2d, Translation2d
+from utils.constants import blueHubLocation
 
 #Constants file
 
@@ -7,6 +9,40 @@ class shooterTargetCmd(Enum): #These are just temporary/example names. We should
     MANUALMODE = 0 #Basically, tell turret to be under the direct control of the operator joysticks.
     CORNERONE = 1 
     CORNERTWO = 2
+
+class shooterTarget():
+
+    def __init__(self, name, targetPos: Translation2d, targetMaxHeight: float, heightOffset: float, spinOffset=0, hubSpin=False, maximizeRoll=False) -> None:
+        """
+        Docstring for __init__
+        
+        :param self: instantiate this for every target position.
+        :param name: name of the target.
+        :param targetPos: based on blue alliance position for the target.
+        :type targetPos: translation2d
+        :param targetMaxHeight: meters- the maximum height you want the ball to reach
+        :type targetHeight: float
+        :param heightOffset: meters- how far from the target you want the ball to reach its maximum height
+        :type heightOffset: float
+        :param spinOffset: how much spin you want -- experimental right now, haven't implemented spin yet 
+        :param hubSpin: if you need to use the spin function 
+        :param maximizeRoll: if you want the ball to roll as far as possible when it reaches the ground. -- for stuff like feeding human player.
+        """
+        self.name = name 
+        self.targetPos = targetPos
+        self.targetHeight = targetMaxHeight
+        self.heightOffset = heightOffset
+        self.spinOffset = spinOffset
+        self.hubSpin = hubSpin
+        self.maximizeRoll = maximizeRoll
+        pass
+    
+
+#Define each target as constants here
+hubTarget = shooterTarget("hub", blueHubLocation, 2.25, .304)
+
+#Throw them in a list here?
+
 
 SHOOTER_OFFSET = .5 #How far the shooter is from the center of the robot in meters.
 #My current unerstanding is that the turret will be in the back center of the robot.
