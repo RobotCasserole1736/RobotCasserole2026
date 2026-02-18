1ec73b64243603a30bf3c05af299453caddfac9c
Kyle <kwpadilla2008@gmail.com>
2026-02-18 07:04:33 -0600

    Updated pitch motor belt ratio to be what cad says. Also messed with the enum values.

--- fuelSystems/fuelSystemConstants.py
+++ fuelSystems/fuelSystemConstants.py
@@ -4,12 +4,14 @@ from utils.units import in2m
 # These are just temporary/example names. We should do better when we decide on actual positions.
 class shooterTargetCmd(Enum):
     MANUALMODE = 0 # Direct control with operator joysticks
-    CORNERONE = 1 
-    CORNERTWO = 2
+    AUTOTARGET = 1
+    CORNERONE = 2
+    CORNERTWO = 3
+    HUB = 4
 
 # How far the shooter is from the center of the robot in meters
 SHOOTER_OFFSET = in2m(-3) # Random number right now
-PITCH_MOTOR_BELT_RATIO = 62
+PITCH_MOTOR_BELT_RATIO = 63.2
 # HOOD_ANGLE_OFFSET = 81
 
 # This is actually the reciprical of the ratio, so in the motor call it will be 1/2 or 1/1
