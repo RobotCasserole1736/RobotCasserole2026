79bf630da3b02bba6ec7d25867a595957dbd3639
Q-Bert <frc1736student@gmail.com>
2026-02-18 20:33:07 -0600

    cleans up shooterCommand

--- Autonomous/commands/shooterCommand.py
+++ Autonomous/commands/shooterCommand.py
@@ -1,9 +1,7 @@
 import os
 import choreo
 import choreo.trajectory
-from wpilib import Timer
 from AutoSequencerV2.command import Command
-from drivetrain.controlStrategies.trajectory import Trajectory
 from utils.allianceTransformUtils import transform
 from utils.autonomousTransformUtils import flip
 from fuelSystems.shooterControl import ShooterController
@@ -13,22 +11,17 @@ class ShooterCommand(Command):
 
     def __init__(self, shootTime_s = 2.0):
             self.name = "ShooterCommand"
-            self.shootTime_s = shootTime_s
-            self.startTime = 1.0
             self.done = False
             self.shooter = ShooterController()
                 
 
     def initialize(self):
-            self.startTime = Timer.getFPGATimestamp()
             self.shooter.toldToShoot = True # Set shooter speed to full power
 
 
     def execute(self):
-        curTime = Timer.getFPGATimestamp() - self.startTime
-        if curTime >= self.shootTime_s:
-         self.shooter.toldToShoot = False # Stop the shooter
-         self.done = True
+        if not self.shooter.toldToShoot:
+            self.done = True
 
     def isDone(self):
         return self.done
