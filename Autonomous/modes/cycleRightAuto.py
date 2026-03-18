a6d6bfedf7e5b93fdc60999ec5272f75c13309e6
ejwatkins <ewatkins987@gmail.com>
2026-03-17 21:14:04 -0500

    Trying to add back auto stuff

--- Autonomous/modes/cycleRightAuto.py
+++ Autonomous/modes/cycleRightAuto.py
@@ -10,22 +10,22 @@ from utils.autonomousTransformUtils import flip
 class CCycleR1(Mode):
     def __init__(self):
         Mode.__init__(self, f"C Cycle R1")
-        
-    #     self.pathCmd1 = DrivePathCommand("fullcycleR1")
-    #     self.pathCmd2 = DrivePathCommand("fullcycleR2")
-    #     self.pathCmd3 = DrivePathCommand("fullcycleR3")
-    #     self.pathCmd4 = DrivePathCommand("fullcycleR4")
-    #     self.score = ShootFuelCommand()
-    #     self.intake = IntakeBallCommand()
-    #     self.group = SequentialCommandGroup([self.score,self.pathCmd1,self.group2,self.pathCmd3])
-    #     self.group2 = ParallelCommandGroup([self.intake,self.pathCmd2])
-    #     self.group2a = ParallelCommandGroup([self.score,self.pathCmd3])
 
-    # def getCmdGroup(self):
-    #     # Just return the path command normally, since we're only doing one path. 
-    #     # When changing to the return self.pathCmd, get rid of the pass
-    #     return self.group
+        self.pathCmd2 = DrivePathCommand("fullcycleR2")
+        self.pathCmd1 = DrivePathCommand("fullcycleR1")
+        self.pathCmd3 = DrivePathCommand("fullcycleR3")
+        self.pathCmd4 = DrivePathCommand("fullcycleR4")
+        self.score = ShootFuelCommand()
+        self.intake = IntakeBallCommand()
+        self.group = SequentialCommandGroup([self.score,self.pathCmd1,self.group2,self.pathCmd3])
+        self.group2 = ParallelCommandGroup([self.intake,self.pathCmd2])
+        self.group2a = ParallelCommandGroup([self.score,self.pathCmd3])
 
-    # def getInitialDrivetrainPose(self):
-    #     # Use the path command to specify the starting pose, using getInitialPose()
-    #     return flip(transform(self.pathCmd1.path.get_initial_pose()))
+    def getCmdGroup(self):
+        # Just return the path command normally, since we're only doing one path.
+        # When changing to the return self.pathCmd, get rid of the pass
+        return self.group
+
+    def getInitialDrivetrainPose(self):
+        # Use the path command to specify the starting pose, using getInitialPose()
+        return flip(transform(self.pathCmd1.path.get_initial_pose()))
--- Autonomous/modes/feedRIghtAuto.py
+++ Autonomous/modes/feedRIghtAuto.py
@@ -7,23 +7,23 @@ from Autonomous.commands.IntakingCommand import IntakeBallCommand
 from utils.allianceTransformUtils import transform
 from utils.autonomousTransformUtils import flip
 
-# class CCycleL1(Mode):
-#     def __init__(self):
-#         Mode.__init__(self, f"C Cycle R1")
+class CCycleL1(Mode):
+    def __init__(self):
+        Mode.__init__(self, f"C Cycle R1")
         
-#         self.pathCmd1 = DrivePathCommand("FeedR1")
-#         self.pathCmd2 = DrivePathCommand("FeedR2")
-#         self.pathCmd3 = DrivePathCommand("FeedR3")
-#         self.score = ShootFuelCommand()
-#         self.intake = IntakeBallCommand()
-#         self.group = SequentialCommandGroup([self.score,self.pathCmd1,self.group2,self.pathCmd3])
-#         self.group2 = ParallelCommandGroup([self.intake,self.pathCmd2])
+        self.pathCmd1 = DrivePathCommand("FeedR1")
+        self.pathCmd2 = DrivePathCommand("FeedR2")
+        self.pathCmd3 = DrivePathCommand("FeedR3")
+        self.score = ShootFuelCommand()
+        self.intake = IntakeBallCommand()
+        self.group = SequentialCommandGroup([self.score,self.pathCmd1,self.group2,self.pathCmd3])
+        self.group2 = ParallelCommandGroup([self.intake,self.pathCmd2])
 
-#     def getCmdGroup(self):
-#         # Just return the path command normally, since we're only doing one path. 
-#         # When changing to the return self.pathCmd, get rid of the pass
-#         return self.group
+    def getCmdGroup(self):
+        # Just return the path command normally, since we're only doing one path. 
+        # When changing to the return self.pathCmd, get rid of the pass
+        return self.group
 
-#     def getInitialDrivetrainPose(self):
-#         # Use the path command to specify the starting pose, using getInitialPose()
-#         return flip(transform(self.pathCmd1.path.get_initial_pose()))
+    def getInitialDrivetrainPose(self):
+        # Use the path command to specify the starting pose, using getInitialPose()
+        return flip(transform(self.pathCmd1.path.get_initial_pose()))
