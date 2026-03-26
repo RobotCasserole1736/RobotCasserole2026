cb5d37d85a84dca0e9df2767ec0aa00c07b55268
Q-Bert <frc1736student@gmail.com>
2026-03-25 19:21:36 -0500

    skelly  for power floor

--- fuelSystems/indexerControl.py
+++ fuelSystems/indexerControl.py
@@ -1,5 +1,5 @@
 from utils.calibration import Calibration
-from utils.constants import INDEXER_CANID
+from utils.constants import INDEXER_CANID, POWER_FLOOR_CANID
 from utils.singleton import Singleton
 from wrappers.wrapperedSparkMax import WrapperedSparkMax
 
@@ -7,10 +7,14 @@ class IndexerControl(metaclass=Singleton):
     def __init__(self):
         self.intakeCommand = False
         self.ejectCommand = False
+        self.powerFloorMotor = WrapperedSparkMax(POWER_FLOOR_CANID, "PowerFloorMotor", brakeMode=False, currentLimitA=20)
         self.indexerMotor = WrapperedSparkMax(INDEXER_CANID, "IndexerMotor", brakeMode=False, currentLimitA=20)
         self.indexerMotorkP = Calibration(name="Indexer kP", default= 0.0)
         self.indexerMotorkV = Calibration(name="Indexer kV", default= 0.005)
+        self.powerFloorMotorkP = Calibration(name="Power floor kP", default=0)
+        self.powerFloorMotorkV = Calibration(name="Power floor kV", default=0)
         self.motorVelCal = Calibration(name="Indexer Velocity", default=12, units="rad/s")
+        self.floorVelCal = Calibration(name="Power Floor Velocity", default=12, units="rad/s")
 
     def update(self):
         if self.indexerMotorkP.isChanged() or self.indexerMotorkV.isChanged():
@@ -19,16 +23,25 @@ class IndexerControl(metaclass=Singleton):
             kI=0,
             kD=0,
             kFF=self.indexerMotorkV.get())
+        if self.powerFloorMotorkP.isChanged() or self.powerFloorMotorkV.isChanged():
+            self.powerFloorMotor.setPIDF(
+            kP=self.powerFloorMotorkP.get(),
+            kI=0,
+            kD=0,
+            kFF=self.powerFloorMotorkV.get())
         if self.ejectCommand:
             self.indexerMotor.setVelCmd(-self.motorVelCal.get())
+            self.powerFloorMotor.setVelCmd(-self.motorVelCal.get())
         elif self.intakeCommand:
             self.indexerMotor.setVelCmd(self.motorVelCal.get())
+            self.powerFloorMotor.setVelCmd(self.motorVelCal.get())
         else:
             self.indexerMotor.setVoltage(0)
+            self.powerFloorMotor.setVoltage(0)
 
     def setIndexerIntake(self, cmd):
         self.intakeCommand = cmd
-
+        
     def setIndexerEject(self, cmd):
         self.ejectCommand = cmd
 
